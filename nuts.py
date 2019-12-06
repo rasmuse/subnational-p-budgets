49a33b1a25ef8168a20b4fb0278f93fa9931f001
Rasmus Einarsson <mr@rasmuseinarsson.se>
2019-12-06 10:17:12 +0100

    Make function for filling NUTS up/down

--- nuts.py
+++ nuts.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import eust
+import fill
 import settings
 
 
@@ -10,6 +11,28 @@ def get_nuts_codes():
 def get_nuts_aggregates():
     nd = get_nuts_codes()
     return {parent: list(g.index) for parent, g in nd.groupby("parent_geo")}
+
+
+def fill_nuts(s):
+    aggregates = get_nuts_aggregates()
+    child_parent_pairs = {
+        children[0]: [parent]
+        for parent, children in aggregates.items()
+        if len(children) == 1
+    }
+    return s.pipe(
+        fill.fill_aggregates,
+        aggregates,
+        level="geo",
+        skipna=False,
+        iterate=True,
+    ).pipe(
+        fill.fill_aggregates,
+        child_parent_pairs,
+        level="geo",
+        skipna=False,
+        iterate=True,
+    )
 
 
 def get_included_regions():
