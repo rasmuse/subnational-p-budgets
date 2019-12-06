abf8cdb08ebb4cf73e40368d9216138df8c71306
Rasmus Einarsson <mr@rasmuseinarsson.se>
2019-12-06 16:51:17 +0100

    Adjust details in grassland yield and mineral rate
    * First, don't use the subset of included NUTS regions in
      the calculation of grassland yields; this is to be determined
      based on, among other things, the coverage of crop yields ---
      so assuming the included NUTS regions is sort of circular reasoning.
    * Second, make some paths into variables in the settings file.

--- settings.py
+++ settings.py
@@ -8,6 +8,11 @@ NUTS_LEVEL_EXCEPTIONS = {
 NUTS_GIS_PATH = "indata/NUTS_RG_20M_2013_3035.shp/NUTS_RG_20M_2013_3035.shp"
 ENZ_GIS_PATH = "indata/enz/enz_v8.shp"
 FADN_GIS_PATH = "indata/FADN_RICA_PL_2012_20M/FADN_RICA_PL_2012_20M.shp"
+FADN_WEIGHTS_PATH = "outdata/fadn_weights_by_nuts.csv"
+ENZ_WEIGHTS_PATH = "outdata/enz_weights_by_nuts.csv"
+GRASSLAND_YIELD_ENZ_PATH = "indata/smit-2008-grassland-yields.csv"
+GRASSLAND_YIELD_NUTS_PATH = "outdata/grassland_yield.csv"
+MINERAL_P_RATE_PATH = "outdata/mineral_p_rate.csv"
 
 GEOPLOT_XLIM = [2636000., 6526000.]
 GEOPLOT_YLIM = [1217000., 5421000.]
